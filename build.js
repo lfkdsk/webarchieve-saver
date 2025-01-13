86843d6292c5359336a3548828fb0d0b26c1bcac
lfkdsk <liufengkai@bytedance.com>
2025-01-13 14:04:16 -0800

    bugfix: with no sandbox.

--- build.js
+++ build.js
@@ -10,7 +10,10 @@ function generateNameFromUrl(url) {
 
 (async () => {
     const path = require('path')
-    const browser = await puppeteer.launch({ headless: 'new' });
+    const browser = await puppeteer.launch({ 
+        headless: 'new',
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
     const page = await browser.newPage();
     await page.setViewport({
         width: 2560,
